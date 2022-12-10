4c6ff4b516e783dab8f049350b40df9b89426e20
manoloacademia <luispablosegovia@icloud.com>
2022-12-10 15:43:25 -0300

    Initial Commit

---
+++ List and Navigation/List_and_NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  List_and_NavigationApp.swift
+//  List and Navigation
+//
+//  Created by Pablo Segovia on 10/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct List_and_NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
